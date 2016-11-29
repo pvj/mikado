4c1784a8af05503ce02db29921143e2f0cf73066
Nandor Sivok <nandor.sivok@gmail.com>
2016-11-29 23:37:59 +0100

    create int.domain internal zone in R53 and added both db instances

--- terraform/wp/r53.tf
+++ terraform/wp/r53.tf
@@ -1,5 +1,19 @@
 resource "aws_route53_zone" "domain" {
   name = "${var.domain}"
+
+  tags {
+    Mikado = "True"
+  }
+}
+
+resource "aws_route53_zone" "int" {
+  name   = "int.${var.domain}"
+  vpc_id = "${var.vpc_id}"
+
+  tags {
+    Environment = "internal"
+    Mikado      = "True"
+  }
 }
 
 resource "aws_route53_record" "apex" {
@@ -13,6 +27,20 @@ resource "aws_route53_record" "apex" {
     zone_id                = "${module.wp-prod-asg.elb_zone_id}"
     evaluate_target_health = true
   }
+}
+
+resource "aws_route53_record" "int-ns" {
+  zone_id = "${aws_route53_zone.domain.zone_id}"
+  name    = "int.${var.domain}"
+  type    = "NS"
+  ttl     = "30"
+
+  records = [
+    "${aws_route53_zone.int.name_servers.0}",
+    "${aws_route53_zone.int.name_servers.1}",
+    "${aws_route53_zone.int.name_servers.2}",
+    "${aws_route53_zone.int.name_servers.3}",
+  ]
 }
 
 resource "aws_route53_record" "www" {
@@ -44,6 +72,30 @@ resource "aws_route53_record" "test" {
   alias {
     name                   = "${module.wp-test-asg.elb_dns_name}"
     zone_id                = "${module.wp-test-asg.elb_zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "rds" {
+  zone_id = "${aws_route53_zone.int.zone_id}"
+  name    = "db.int.${var.domain}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_db_instance.wp-prod.address}"
+    zone_id                = "${aws_db_instance.wp-prod.hosted_zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "test-rds" {
+  zone_id = "${aws_route53_zone.int.zone_id}"
+  name    = "test.db.int.${var.domain}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_db_instance.wp-test.address}"
+    zone_id                = "${aws_db_instance.wp-test.hosted_zone_id}"
     evaluate_target_health = true
   }
 }
